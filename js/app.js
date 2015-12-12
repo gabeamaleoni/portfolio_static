7e3613de90232b400f3cfe51ac8c7cea0bbf3334
gabriel leoni <gabeamaleoni@gmail.com>
2015-12-11 20:12:13 -0800

    Added responsive structure and styling

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//Scroll to #id effect
+
+$('a[href^="#"]').on('click', function(event) {
+	var target = $( $(this).attr('href') );
+
+	if( target.length ) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: target.offset().top
+		}, 400);
+	}
+});
