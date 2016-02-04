769e55c1aa03b9446d52b1a0304967cd9fe0326a
gabriel leoni <gabeamaleoni@gmail.com>
2016-02-04 13:15:58 -0800

    add contact section to each page

--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,1 +1,1 @@
-$('a[href^="#"]').on("click",function(t){var e=$($(this).attr("href"));e.length&&(t.preventDefault(),$("html, body").animate({scrollTop:e.offset().top},400))});
+$('a[href^="#"]').on("click",function(t){var e=$($(this).attr("href"));e.length&&(t.preventDefault(),$("html, body").animate({scrollTop:e.offset().top-115},400))});
